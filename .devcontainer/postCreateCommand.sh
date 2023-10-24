84de12957f5b356b7aa9083ebef2c4fe041e53ff
Matt Mazzola <mattm@microsoft.com>
2023-10-24 21:25:54 +0000

    Add install instructions in post create command

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -2,5 +2,13 @@ git config --global safe.directory '*'
 git config --global core.editor "code --wait"
 git config --global pager.branch false
 
+# Install package locally
+conda activate llava
+pip install --upgrade pip  # enable PEP 660 support
+pip install -e .
+
+# Install additional packages for training
+pip install -e ".[train]"
+pip install flash-attn --no-build-isolation
 
 echo "postCreateCommand.sh COMPLETE!"
