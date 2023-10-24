6bf3074374b5c15eed7bc8e37fc9aeaf928a3196
Matt Mazzola <mattm@microsoft.com>
2023-10-24 22:49:47 +0000

    Fix command order

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -6,25 +6,27 @@ git config --global pager.branch false
 echo "export AZCOPY_CONCURRENCY_VALUE=AUTO" >> ~/.zshrc
 echo "export AZCOPY_CONCURRENCY_VALUE=AUTO" >> ~/.bashrc
 
-source /home/vscode/miniconda3/bin/activate
-
-# Create and activate llava environment
-conda create -y -q -n llava python=3.10
-conda activate llava
-
+# Activate conda by default
 echo ". /home/vscode/miniconda3/bin/activate" >> ~/.zshrc
 echo ". /home/vscode/miniconda3/bin/activate" >> ~/.bashrc
+
+# Use llava environment by default
 echo "conda activate llava" >> ~/.zshrc
 echo "conda activate llava" >> ~/.bashrc
 
-pip install pre-commit==3.0.2
-
+# Add dotnet to PATH
 echo 'export PATH="$PATH:$HOME/.dotnet"' >> ~/.bashrc
 echo 'export PATH="$PATH:$HOME/.dotnet"' >> ~/.zshrc
 
-# Setup CUDA_HOME for deepspeed
-echo "export CUDA_HOME=/usr/lib/x86_64-linux-gnu/" >> ~/.bashrc
-echo "export CUDA_HOME=/usr/lib/x86_64-linux-gnu/" >> ~/.zshrc
+# Create and activate llava environment
+source /home/vscode/miniconda3/bin/activate
+conda create -y -q -n llava python=3.10
+conda activate llava
+
+# Install Nvidia Cuda Compiler
+conda install -y -c nvidia cuda-compiler
+
+pip install pre-commit==3.0.2
 
 # Install package locally
 pip install --upgrade pip  # enable PEP 660 support
